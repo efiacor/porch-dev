1c7652d0a6a95735f0d5cc34fda74ad43cd7b15a
Fiachra Corcoran <fiachra.corcoran@est.tech>
2024-05-28 13:19:37 +0100

    Update coverage stats

--- default-go-test.mk
+++ default-go-test.mk
@@ -29,12 +29,13 @@ ifeq ($(CONTAINER_RUNNABLE), 0)
 		$(RUN_CONTAINER_COMMAND) docker.io/nephio/gotests:1782782171367346176 \
         sh -e -c "git config --global --add user.name test; \
 	 	git config --global --add user.email test@nephio.org; \
-	 	go test ./... -v -coverprofile ${TEST_COVERAGE_TMP_FILE}; \
-		cat ${TEST_COVERAGE_TMP_FILE} | grep -v "test" > ${TEST_COVERAGE_FILE}; \
+	 	go test ./... -v -covermode=atomic -coverpkg=./... -coverprofile ${TEST_COVERAGE_TMP_FILE}; \
+		cat ${TEST_COVERAGE_TMP_FILE} | grep -v 'test\|api' > ${TEST_COVERAGE_FILE}; \
         go tool cover -html=${TEST_COVERAGE_FILE} -o ${TEST_COVERAGE_HTML_FILE}; \
         go tool cover -func=${TEST_COVERAGE_FILE} -o ${TEST_COVERAGE_FUNC_FILE}"
 else
-		go test ./... -v -coverprofile ${TEST_COVERAGE_FILE}
+		go test ./... -v -covermode=atomic -coverpkg=./... -coverprofile ${TEST_COVERAGE_TMP_FILE}; \
+		cat ${TEST_COVERAGE_TMP_FILE} | grep -v 'test\|api' > ${TEST_COVERAGE_FILE}; \
 		go tool cover -html=${TEST_COVERAGE_FILE} -o ${TEST_COVERAGE_HTML_FILE}
 		go tool cover -func=${TEST_COVERAGE_FILE} -o ${TEST_COVERAGE_FUNC_FILE}
 endif
